c7f88e86d3e695833c62c1affe515a702a297aca
BeatyPete <akira941@gmail.com>
2020-11-09 04:32:11 -0600

    added uv api and script to display current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,11 @@ var citySubmit = function(event) {
 };
 
 var getWeather = function(cityName) {
-    var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=221a2ad7e76ce710f1907c1731e24ff2`;
+    var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=221a2ad7e76ce710f1907c1731e24ff2`;
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
           response.json().then(function(data) {
-              displayCurrent(data);
+              getUV(data);
           });
         } else {
           alert("Error: " + response.statusText);
@@ -20,10 +20,59 @@ var getWeather = function(cityName) {
       });
 };
 
-var displayCurrent = function(cityWeather) {
-    console.log(cityWeather);
-    //display cities name
-    
+var getUV = function(cityWeather) {
+    var lat = cityWeather.coord.lat;
+    var lon = cityWeather.coord.lon;
+    var apiUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=221a2ad7e76ce710f1907c1731e24ff2`;
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+          response.json().then(function(data) {
+              displayCurrent(cityWeather, data);
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      });
+};
+
+var displayCurrent = function(cityWeather, uv) {
+    currWeatherEl.innerHTML = "";
+    console.log(uv);
+    //get date for city and turn into moment
+    var date = moment.unix(cityWeather.dt).format("(M/DD/YYYY)");
+    //display city name and current date
+    var cityNameEl = document.createElement("h2");
+    cityNameEl.classList = "card-title";
+    cityNameEl.innerHTML = `${cityWeather.name} ${date}`;
+    currWeatherEl.appendChild(cityNameEl);
+    //display temp
+    var tempEl = document.createElement("p");
+    tempEl.innerHTML = `Temperature: ${cityWeather.main.temp} &deg;F`;
+    currWeatherEl.appendChild(tempEl);
+    //display humidity
+    var humidEl = document.createElement("p");
+    humidEl.innerHTML = `Humidity: ${cityWeather.main.humidity}%`;
+    currWeatherEl.appendChild(humidEl);
+    //display wind
+    var windEl = document.createElement("p");
+    windEl.innerHTML = `Wind Speed: ${cityWeather.wind.speed} MPH`;
+    currWeatherEl.appendChild(windEl);
+    //display uv
+    var uvEl = document.createElement("p");
+    uvEl.innerHTML = "UV Index: ";
+    var uvBadge = document.createElement("span");
+    uvBadge.innerHTML = uv.value;
+    uvEl.appendChild(uvBadge);
+    if (uv.value < 3) {
+        uvBadge.classList = "badge bg-success";
+    }
+    else if (uv.value >= 3 && uv.value < 7) {
+        uvBadge.classList = "badge bg-warning";
+    }
+    else if (uv.value >= 7) {
+        uvBadge.classList = "badge bg-danger";
+    };
+    currWeatherEl.appendChild(uvEl);
 };
 
 
